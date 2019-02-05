1e68f18b4e732570b9dd8c25a092f594f4817a38
Alexander Shmakov <ashmakov@uci.edu>
2019-02-04 18:25:22 -0800

    Completed turned-based decorator

--- spacetimerl/turn_based_environment.py
+++ spacetimerl/turn_based_environment.py
@@ -1,7 +1,7 @@
 from spacetimerl.base_environment import BaseEnvironment
 
 from abc import ABC, abstractmethod
-from typing import Tuple, List, Union
+from typing import Tuple, List, Union, Dict, Type
 import numpy as np
 
 
@@ -18,10 +18,16 @@ class TurnBasedEnvironment(ABC):
         """ Property holding the max number of players present for a game. """
         raise NotImplementedError
 
+    @staticmethod
+    @abstractmethod
+    def observation_names() -> List[str]:
+        """ Static method for returning the names of the observation objects """
+        raise NotImplementedError
+
     @property
     @abstractmethod
-    def observation_shape(self) -> tuple:
-        """ Property holding the numpy shape of a transformed observation state. """
+    def observation_shape(self) -> Dict[str, tuple]:
+        """ Maps each observation name to a numpy shape"""
         raise NotImplementedError
 
     @abstractmethod
@@ -52,62 +58,66 @@ class TurnBasedEnvironment(ABC):
         """ Valid actions for a specific state. """
         raise NotImplementedError
 
-    def state_to_observation(self, state: object, player: int) -> np.ndarray:
-        """ Convert the raw game state to the observation for the agent.
-        The observation must be able to be fed into your predictor.
+    def state_to_observation(self, state: object, player: int) -> Dict[str, np.ndarray]:
+        """ Convert the raw game state to the observation for the agent. Maps each observation name into an observation.
 
         This can return different values for the different players. Default implementation is just the identity."""
-        return np.array(state)
+        return {'state': np.array(state)}
 
 
-class TurnBasedWrapper(BaseEnvironment):
-    def __init__(self, base: TurnBasedEnvironment):
-        self.base = base
+def turned_based_environment(cls: Type[TurnBasedEnvironment]):
+    class TurnBasedWrapper(BaseEnvironment):
+        def __init__(self, *args, **kwargs):
+            self.base = cls(*args, **kwargs)
 
-    @property
-    def min_players(self) -> int:
-        return self.base.min_players
+        @property
+        def min_players(self) -> int:
+            return self.base.min_players
 
-    @property
-    def max_players(self) -> int:
-        return self.base.max_players
+        @property
+        def max_players(self) -> int:
+            return self.base.max_players
 
-    @property
-    def state_shape(self) -> tuple:
-        return self.base.state_shape
+        @staticmethod
+        def observation_names() -> List[str]:
+            """ Static method for returning the names of the observation objects """
+            return cls.observation_names()
 
-    @property
-    def observation_shape(self) -> tuple:
-        return self.base.observation_shape
+        @property
+        def observation_shape(self) -> Dict[str, tuple]:
+            """ Maps each observation name to a numpy shape"""
+            return self.base.observation_shape
 
-    def new_state(self, num_players: int = 1) -> Tuple[object, List[int]]:
-        return (num_players, self.base.new_state(num_players)), [0]
+        def new_state(self, num_players: int = 1) -> Tuple[object, List[int]]:
+            return (num_players, self.base.new_state(num_players)), [0]
 
-    def next_state(self, state: object, players: [int], actions: [str]) \
-            -> Tuple[object, List[int], List[float], bool, Union[List[int], None]]:
-        num_players, state = state
-        player: int = players[0]
-        action: str = actions[0]
+        def next_state(self, state: object, players: [int], actions: [str]) \
+                -> Tuple[object, List[int], List[float], bool, Union[List[int], None]]:
+            num_players, state = state
+            player: int = players[0]
+            action: str = actions[0]
 
-        state, reward, terminal, winners = self.base.next_state(state, player, action)
+            state, reward, terminal, winners = self.base.next_state(state, player, action)
 
-        state = (num_players, state)
-        new_players = [(player + 1) % num_players]
-        rewards = [reward]
-        winners = winners
+            state = (num_players, state)
+            new_players = [(player + 1) % num_players]
+            rewards = [reward]
+            winners = winners
 
-        return state, new_players, rewards, terminal, winners
+            return state, new_players, rewards, terminal, winners
 
-    def valid_actions(self, state: object, player: int) -> [str]:
-        num_players, state = state
-        return self.base.valid_actions(state, player)
+        def valid_actions(self, state: object, player: int) -> [str]:
+            num_players, state = state
+            return self.base.valid_actions(state, player)
 
-    def is_valid_action(self, state: object, player: int, action: str) -> bool:
-        num_players, state = state
-        return self.base.is_valid_action(state, player, action)
+        def is_valid_action(self, state: object, player: int, action: str) -> bool:
+            num_players, state = state
+            return self.base.is_valid_action(state, player, action)
 
-    def state_to_observation(self, state: object, player: int) -> np.ndarray:
-        num_players, state = state
-        return self.base.state_to_observation(state, player)
+        def state_to_observation(self, state: object, player: int) -> Dict[str, np.ndarray]:
+            num_players, state = state
+            return self.base.state_to_observation(state, player)
+
+    return TurnBasedWrapper
 
 
