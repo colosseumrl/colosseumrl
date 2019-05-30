8ede028289459a01da585a25bd925940b93e5573
John Lanier <johnblanier@gmail.com>
2019-05-30 16:56:16 -0700

    Forgot cythonize command

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
 from setuptools import Extension, setup
-from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 with open("README.md", "r") as fh:
@@ -21,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/colosseumrl/Colosseum",
     packages=setuptools.find_packages(),
-    ext_modules=extensions,
+    ext_modules=cythonize(extensions),
     classifiers=[
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
