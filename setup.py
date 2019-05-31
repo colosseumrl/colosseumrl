20434177abb7b5e2148b09e230e50cedecab1f0e
John Lanier <johnblanier@gmail.com>
2019-05-30 17:33:38 -0700

    More setup py action

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 import setuptools
 from setuptools import Extension, setup
+from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extensions = [
-    Extension("CyTronGrid", ["colosseumrl/envs/tron/CyTronGrid.pyx"],
+    Extension("colosseumrl.envs.tron.CyTronGrid", ["colosseumrl/envs/tron/CyTronGrid.pyx"],
               extra_compile_args=["-O3", "-march=native"])
 ]
+
+
+with open("requirements.txt", 'r') as file:
+    requirements = file.readlines()
+
+requirements = list(map(str.strip, requirements))
 
 setup(
     name="colosseumrl",
@@ -21,6 +28,8 @@ setup(
     url="https://github.com/colosseumrl/Colosseum",
     packages=setuptools.find_packages(),
     ext_modules=cythonize(extensions),
+    cmdclass={'build_ext': build_ext},
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
