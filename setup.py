9d8460e5129992c22bf15282ea99b237bb48099a
Alexander Shmakov <Alexanders101@gmail.com>
2019-05-30 19:39:35 -0700

    Final PyPI edit for cython support and nice compilation.

--- setup.py
+++ setup.py
@@ -1,25 +1,45 @@
 import setuptools
+import sys
 from setuptools import Extension, setup
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
 
+# Readme autoload
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-extensions = [
-    Extension("colosseumrl.envs.tron.CyTronGrid", ["colosseumrl/envs/tron/CyTronGrid.pyx"],
-              extra_compile_args=["-O3", "-march=native"])
-]
-
-
+# Requirements autoload
 with open("requirements.txt", 'r') as file:
     requirements = file.readlines()
 
 requirements = list(map(str.strip, requirements))
 
+# Cython setup
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+if '--without-cython' in sys.argv:
+    use_cython = False
+    sys.argv.remove('--without-cython')
+
+cmdclass = { }
+ext_modules = [ ]
+
+if use_cython:
+    ext_modules += [
+        Extension("colosseumrl.envs.tron.CyTronGrid", [ "colosseumrl/envs/tron/CyTronGrid.pyx" ]),
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("colosseumrl.envs.tron.CyTronGrid", [ "colosseumrl/envs/tron/CyTronGrid.c" ]),
+    ]
+
 setup(
     name="colosseumrl",
-    version="1.0.0",
+    version="1.0.2",
     author="Alexander Shmakov",
     author_email="alexanders101@gmail.com",
     description="UC Irvine multi-agent reinforcement learning framework",
@@ -27,7 +47,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/colosseumrl/Colosseum",
     packages=setuptools.find_packages(),
-    ext_modules=cythonize(extensions),
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.5",
